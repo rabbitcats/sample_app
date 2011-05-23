bf81ab73e11d240c16c96f8cac739d87cd13a15d
Jacqueline Tong <jtong2002@yahoo.com>
2011-05-23 09:13:15 -0700

    Finished layout and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 SampleApp::Application.routes.draw do
-  #get "pages/home"
+  get "users/new"
 
-  #get "pages/contact"
+  get "pages/home"
 
-  #get "pages/about"
+  get "pages/contact"
+
+  get "pages/about"
   
-  #get "pages/help"
+  get "pages/help"
   
-  root :to => "pages#home"
+  root :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
